7991b578a6dbd118c00301b6df0c7561e42dd871
Vaibhav Choudhary <vaibhavch29@gmail.com>
2023-09-25 20:37:26 +0530

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material';
+import './header.css';
+
+export const Header = () => {
+    return (
+        <div class='header'>
+            <div id='logo'>
+                <Button variant="text">Text</Button>
+            </div>
+
+            <div id='heading'>
+                
+            </div>
+        </div>
+    )
+}
