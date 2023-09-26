dcda3dc8486714ca73ac52b131ab20272396c9bc
Vaibhav Choudhary <vaibhavch29@gmail.com>
2023-09-26 10:33:57 +0530

    view certi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/HomePage/HomePage';
+import ViewCerti from './pages/ViewCerti/ViewCerti';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element = { <Home/> } exact />
+        <Route path='/view' element = { <ViewCerti/> } exact/>
       </Routes>
     </div>
   );
